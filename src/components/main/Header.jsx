ed894190f7adea60e49e2b217e269ebdf77c3736
Heesu Jang <heesu52@naver.com>
2024-07-11 23:00:22 +0900

    design: 자동슬라이드될 때 처음으로 돌아가는 부분 수정

--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
                     <Lottiefile/>
                     <a href="/" className="text-[28px] text-custom-indigo font-bold">SML</a>
                 </div>
-                <div className="flex w-[170px] h-[80px] items-center justify-between mr-[80px] p-[10px]">
+                <div className="flex w-[190px] h-[80px] items-center justify-between mr-[80px] p-[10px]">
                     <a href="/signup" className="text-[20px] text-custom-indigo font-bold">Sign Up</a>
                     <a href="/login" className="text-[20px] text-custom-indigo font-bold">Login</a>
                 </div>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -10,7 +10,7 @@ const useInterval = (callback, interval) => {
 
   useEffect(() => {
     savedCallback.current = callback;
-  });
+  }, [callback]);
 
   useEffect(() => {
     function tick() {
@@ -26,28 +26,55 @@ const useInterval = (callback, interval) => {
 };
 
 function MainPage() {
-  const COPIED_NUM = 3; //슬라이드 수
-  const [slideIndex, setSlideIndex] = useState(0); //슬라이드 인덱스
-  const slideRef = useRef(null); //슬라이드 컨테이너 참조 -> 동적으로 제어하기위해
-  const [custominterval, setCustominterval] = useState(4000); 
+  const SLIDE_NUM = 3; //슬라이드 수
+  const [slideIndex, setSlideIndex] = useState(1); // 1로 시작
+  const slideRef = useRef(null);
+  const [customInterval, setCustomInterval] = useState(4000); //슬라이드가 4초씩 이동
 
-  const slideHandler = (direction) => { //버튼 클릭시 슬라이드 이동
-    setSlideIndex((prevIndex) => (prevIndex + direction + COPIED_NUM) % COPIED_NUM);
+  const slideHandler = (direction) => { //버튼 클릭 시 슬라이드 이동, -1은 왼쪽 +1은 오른쪽
+    setSlideIndex((prevIndex) => prevIndex + direction);
   };
 
   useEffect(() => {
     if (slideRef.current) {
-      slideRef.current.style.transition = "all 500ms ease-in-out";
-      slideRef.current.style.transform = `translateX(${-1 * ((100 / COPIED_NUM) * slideIndex)}%)`;
-    }}, [slideIndex]);
+      slideRef.current.style.transition = "all 500ms ease-in-out"; //이미지 변경 효과(0.5초동안)
+      slideRef.current.style.transform = `translateX(${-1 * (100 / (SLIDE_NUM + 2)) * slideIndex}%)`;//하나의 슬라이드(20%)만큼 이동
+    }
+  }, [slideIndex]);
 
-  useInterval(
-  () => setSlideIndex((slideIndex) => slideIndex + 1),custominterval);
+  useInterval(() => setSlideIndex((slideIndex) => slideIndex + 1), customInterval); //자동 슬라이드
 
-  if (slideIndex === 3) {
-  setSlideIndex(0)
-  }
-  
+  useEffect(() => {
+    if (slideIndex === 0) { //클론된 마지막 슬라이드
+      setTimeout(() => {
+        if (slideRef.current) {
+          slideRef.current.style.transition = "none"; //클론 슬라이드 이므로 애니메이션 비활성화
+          setSlideIndex(SLIDE_NUM); //클론된 슬라이드와 같은 마지막 슬라이드로 설정
+          slideRef.current.style.transform = `translateX(${-1 * (100 / (SLIDE_NUM + 2)) * SLIDE_NUM}%)`; //마지막 슬라이드로 이동
+        }
+      }, 500);
+    } else if (slideIndex === SLIDE_NUM + 1) { //클론된 첫 슬라이드
+      setTimeout(() => {
+        if (slideRef.current) {
+          slideRef.current.style.transition = "none"; 
+          setSlideIndex(1); // 첫번째 슬라이드로 이동
+          slideRef.current.style.transform = `translateX(${-1 * (100 / (SLIDE_NUM + 2)) * 1}%)`;
+        }
+      }, 500);
+    } else {
+      if (slideRef.current) {
+        slideRef.current.style.transition = "all 500ms ease-in-out";
+      }
+    }
+  }, [slideIndex, SLIDE_NUM]);
+
+  useEffect(() => {
+    if (slideIndex === 4) { // 마지막 인덱스
+      setCustomInterval(1000); //클론슬라이드이므로 기존의 2배속도로 이미지 효과 
+    } else {
+      setCustomInterval(4000); //그게 아니면 4초씩 이동
+    }
+  }, [slideIndex]);
 
   return (
     <>
@@ -55,9 +82,12 @@ function MainPage() {
       <div className="relative w-full overflow-hidden">
         <div
           ref={slideRef}
-          className="flex"
-          style={{ width: `${100 * COPIED_NUM}vw`}}
+          className="flex transition-transform duration-500 ease-in-out"
+          style={{ width: `${100 * (SLIDE_NUM + 2)}%` }}
         >
+          <div className="flex w-full h-full"> 
+            <Mainthird />
+          </div>
           <div className="flex w-full h-full">
             <Mainfirst />
           </div>
@@ -66,6 +96,9 @@ function MainPage() {
           </div>
           <div className="flex w-full h-full">
             <Mainthird />
+          </div>
+          <div className="flex w-full h-full">
+            <Mainfirst />
           </div>
         </div>
         <button
