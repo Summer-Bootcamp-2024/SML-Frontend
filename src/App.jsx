b410a1e3af2adf01e2eab136a6a4382fc819fcc3
yellowmodric <87848941+yellowmodric@users.noreply.github.com>
2024-07-11 15:58:09 +0900

    [#6]Design : 회원가입 페이지 퍼블리싱

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
     </BrowserRouter>
   )
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
-function Button() {
+function Button({label}) {
     return (
         <div className="flex justify-center items-center text-stone-50 text-xl font-extrabold tracking-tight w-[150px] h-[50px] bg-gray-600 rounded-[10px] border border-zinc-300 cursor-pointer">
-            Start
+            {label}
         </div>
     )
 }
