ab8fa5525f062c906a1c89ac814057c6cd9570e3
Heesu Jang <heesu52@naver.com>
2024-07-13 02:08:06 +0900

    deisgn: 일촌 목록 리스트 페이지 퍼블리싱

---
+++ src/components/FriendList.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function FriendList() {
+  return (
+    <div className="w-full h-[550px] flex justify-center items-center">
+      <div className="w-[650px] h-[500px] bg-custom-white rounded-[10px] overflow-y-auto">
+        <div className="flex flex-col justify-center items-center mt-[20px]">
+          {[...Array(10)].map((_, index) => (
+            <div className="w-[600px] h-[65px] flex justify-center items-center" key={index}>
+              <div className="flex items-center justify-between w-[500px] p-[10px] border-b-[1px] border-custom-grey">
+                <div className="text-[16px] h-[24px] font-semibold ">김대희</div>
+                <div className="flex items-center justify-center w-[350px] p-[5px]">
+                  <div className="text-[16px] mr-[10px]">백엔드 개발자</div>
+                  <div className="text-[16px] mr-[10px]">실리콘밸리</div>
+                  <div className="text-[16px] mr-[10px]">서울시 중구</div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FriendList;
---
+++ src/pages/FriendListPage.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useRef, useEffect } from 'react';
+import Sidebar from '../components/Sidebar';
+import FriendGraph from '../components/FriendGraph';
+import FriendList from '../components/FriendList';
+
+
+
+function FriendListPage() {
+  const [sortOption, setSortOption] = useState('graph')
+  
+  const Sortoption = [
+    { value: 'graph', name: 'Friend Graph' },
+    { value: 'list', name: 'Friend List' },
+  ]
+  
+  const onListHandler = (e) => {
+    setSortOption(e.target.value)
+    console.log(sortOption)
+  }
+  
+  return (
+   <div className='flex'>
+    <Sidebar/>
+    <div className='flex flex-col justify-center w-[calc(100vw-296px)] h-screen border-2 border-blue-400'>
+      <select
+      className='w-[170px] h-[45px] border-[1px] border-custom-grey bg-custom-white rounded-[10px] mt-[-20px] ml-[40px] font-semibold text-[18px] text-custom-indigo indent-[10px]'
+      onChange={onListHandler}
+      >
+        {Sortoption.map((item) => (
+          <option value={item.value} key={item.name} className='text-[18px]'>
+            {item.name}
+          </option>
+        ))}
+      </select>
+      <div className='flex items-center justify-center w-full'>
+      {sortOption ==='graph' ? <FriendGraph/> : <FriendList/>}
+      </div>
+    </div>
+   </div>
+  );
+}
+
+export default FriendListPage;
