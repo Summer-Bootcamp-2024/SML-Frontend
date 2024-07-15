d2f2ad152b79f90f857700a002efa9f631d023d8
Heesu Jang <heesu52@naver.com>
2024-07-16 00:52:46 +0900

    feat: 상태관리 추가

---
+++ src/store/store.js
@@ -0,0 +1,15 @@
+import { create } from 'zustand';
+
+/**
+ * Zustand 상태 관리를 위한 store를 생성합니다.
+ * 이 store는 `apiUrl` 상태와 `setApiUrl` 액션을 관리합니다.
+ * 
+ * `apiUrl`은 API 요청을 보낼 기본 URL을 저장합니다.
+ * `setApiUrl`은 `apiUrl` 상태를 업데이트하는 함수입니다.
+ */
+export const useApiUrlStore = create((set) => ({
+
+  apiUrl: 'https://localhost:8000/',
+
+  setApiUrl: (url) => set((state) => ({ ...state, apiUrl: url })),
+}));
