22cebafd625dfc74df78b45dd67d983e8bdd412a
Heesu Jang <heesu52@naver.com>
2024-07-19 16:37:57 +0900

    feat:채팅기능구현

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -20,9 +20,27 @@ function Chat({ selectedRoom, friendstatus }) {
       };
 
       socket.onmessage = (e) => {
-        const parsedData = JSON.parse(e.data);
-        console.log("백엔드에서 송신된 데이터:", parsedData);
-        setMessages((prevMessages) => [...prevMessages, parsedData]);
+        try {
+          const parsedData = JSON.parse(e.data);
+          let content = parsedData.content;
+
+          if (typeof content === 'string') {
+            try {
+              const contentObj = JSON.parse(content);
+              content = contentObj.content || content; // contentObj가 content 필드를 가질 경우 그것을 사용
+            } catch (error) {
+              console.error("Error parsing nested JSON:", error);
+            }
+          }
+
+          const messageData = {
+            ...parsedData,
+            content: content,
+          };
+          setMessages((prevMessages) => [...prevMessages, messageData]);
+        } catch (error) {
+          console.error("Error parsing WebSocket message:", error);
+        }
       };
 
       socket.onclose = () => {
@@ -46,15 +64,22 @@ function Chat({ selectedRoom, friendstatus }) {
   const sendMessage = (messageContent) => {
     if (messageContent.trim() !== '') {
       const newMessage = {
-        content: messageContent,
-        sender: `${user_id}`,
+        type: 'message',
+      
+          content: messageContent,
+      
+        client_id: user_id,
         withButton: friendstatus,
       };
 
       if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
         socketRef.current.send(JSON.stringify(newMessage));
-        setMessages((prevMessages) => [...prevMessages, newMessage]);
       }
+
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        newMessage // 보낸 메시지를 배열에 추가
+      ]);
 
       setInputMessage('');
     }
@@ -101,35 +126,17 @@ function Chat({ selectedRoom, friendstatus }) {
     }
   };
 
-  const createChatRoom = async () => {
-    if (status === 'accepted') {
-      const id = {
-        user1_id: user_id,
-        user2_id: selectedRoom.user2_id,
-      };
-      try {
-        const response = await axios.post(`${apiUrl}/chatrooms`, id, {
-          withCredentials: true,
-        });
-        console.log(response.data);
-      } catch (error) {
-        console.error('Error creating chat room:', error);
-        alert('채팅방 생성에 실패했습니다');
-      }
-    }
-  };
-
   const handleFriendStatusUpdate = async (newStatus) => {
     setStatus(newStatus);
     await updateFriendStatus();
     if (newStatus === 'accepted') {
-      await createChatRoom();
+      // await createChatRoom();
     }
   };
 
   useEffect(() => {
     getChatHistory();
-    createChatRoom();
+    // createChatRoom();
   }, [selectedRoom.room_id]);
 
   const handleKeyDown = (event) => {
@@ -151,8 +158,8 @@ function Chat({ selectedRoom, friendstatus }) {
           </div>
           <div className="flex flex-col justify-end w-full h-[440px] overflow-y-auto p-[10px]">
             {messages.map((message, index) => (
-              <div key={index} className={`flex items-center w-full my-[4px] ${message.sender === `${user_id}` ? 'justify-end' : 'justify-start'}`}>
-                <div className={`flex items-center max-w-[80%] p-[10px] ${message.sender === `${user_id}` ? 'bg-custom-blue text-custom-white' : 'bg-custom-white text-black'} rounded-[20px] text-[16px]`}>
+              <div key={index} className={`flex items-center w-full my-[4px] ${message.client_id === user_id ? 'justify-end' : 'justify-start'}`}>
+                <div className={`flex items-center max-w-[80%] p-[10px] ${message.client_id === user_id ? 'bg-custom-blue text-custom-white' : 'bg-custom-white text-black'} rounded-[20px] text-[16px]`}>
                   {message.content}
                   {message.withButton && (
                     <div>
