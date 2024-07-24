652d8fc939d720d210d59c50b2aa8051b83083bf
Heesu Jang <heesu52@naver.com>
2024-07-24 21:17:46 +0900

    chore: yarn build 시 발생하는 에러 및 주의사항 해결

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +13,35 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    },
+    // 추가된 terserOptions
+    terserOptions: {
+      compress: {
+        // eval 사용을 막기 위한 설정
+        unsafe_eval: false,
+      },
+      mangle: {
+        keep_classnames: true,
+        keep_fnames: true,
+      },
+      format: {
+        comments: false,
+      },
+    }
   }
-})
+});
