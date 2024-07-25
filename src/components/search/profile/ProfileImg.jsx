37b095fc9b4674db68b9b0da7df8ccfbe41c4300
yellowmodric <87848941+yellowmodric@users.noreply.github.com>
2024-07-26 03:40:56 +0900

    hotfix : 프로필 카드 이미지 변경

--- src/components/search/profile/ProfileImg.jsx
+++ src/components/search/profile/ProfileImg.jsx
@@ -1,6 +1,28 @@
+import { useApiUrlStore, useUserIdStore } from "../../../store/store";
+import basicProfile from '../../../assets/images/myprofile/basicProfile.png';
+import { useState, useEffect } from "react";
 function ProfileImg({image_url}) {
+    const [profile, setProfile] = useState({});
+    const { apiUrl} = useApiUrlStore();
+    const { user_id } = useUserIdStore();
+
+    const getProfile = async () => {
+        try {
+            const response = await axios.get(`${apiUrl}/users/${user_id}`, {
+                withCredentials: true,
+            });
+            setProfile(response.data);
+        } catch (err) {
+            console.error('Error fetching profile data:', err);
+        }
+    }
+
+    useEffect(() => {
+        getProfile();
+    }, []);
+
     return (
-        <img src={image_url}
+        <img src={profile.image_url || basicProfile}
         className="w-[100px] h-[100px] rounded-full" />
     )
 }
