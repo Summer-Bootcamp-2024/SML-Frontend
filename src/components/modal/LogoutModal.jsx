dabc66ea1d11d17f4d8c23e725fe481718834d41
yellowmodric <87848941+yellowmodric@users.noreply.github.com>
2024-07-26 01:48:18 +0900

    [#103]Design: 로그아웃 모달 수정

--- src/components/modal/LogoutModal.jsx
+++ src/components/modal/LogoutModal.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from "../Button";
 import CheckLottie from "../lottie/checklottie.jsx";
 import FailLottie from "../lottie/faillottie.jsx";
-
+import ReactDOM from "react-dom";
 function LogoutModal({ onClose, logoutSuccess, navigate }) {
 
     const handleFailure = () => {
@@ -13,9 +13,9 @@ function LogoutModal({ onClose, logoutSuccess, navigate }) {
         onClose();
         navigate('/'); 
     };
-    
-    return (
-        <div className={`fixed top-0 flex items-center justify-center w-full min-h-screen border-2 bg-white/50 backdrop-blur-sm`}>
+
+    const modalContent = (
+        <div className={`fixed top-0 flex items-center justify-center w-full min-h-screen border-2 bg-white/50 backdrop-blur-sm z-20`}>
             <div className=" flex flex-col items-center justify-center w-[400px] h-[250px] bg-custom-white rounded-[10px] border-[1px] border-custom-grey">
                 {logoutSuccess ? (
                     <div className='flex flex-col items-center'>
@@ -42,6 +42,8 @@ function LogoutModal({ onClose, logoutSuccess, navigate }) {
             </div>
         </div>
     );
+
+    return ReactDOM.createPortal(modalContent, document.body);
 }
 
 export default LogoutModal;
