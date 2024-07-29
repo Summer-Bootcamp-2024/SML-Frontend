cd0c3bcb917c1cac1dcc8b1fb06abed0cbdcbcbf
Heesu Jang <heesu52@naver.com>
2024-07-29 15:30:02 +0900

    design: 로고 변경

--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -1,12 +1,13 @@
-import Lottiefile from "../lottie/Logolottie"
+import Logo from "../../assets/images/LogoImg.png"
 
 function Header() {
     return(
         <div>
             <div className="flex w-full h-[80px]items-center justify-between font-[GmarketSansMedium]">
-                <div className="flex w-[175px] h-[80px] items-center justify-between p-[10px] ml-[80px]">
-                    <Lottiefile/>
-                    <a href="/" className="text-[28px] text-custom-indigo font-bold">SML</a>
+                <div className="flex h-[80px] items-center justify-between p-[10px] ml-[80px]">
+                    <a href="/">
+                        <img src={Logo} className="w-[230px]"/>
+                    </a>
                 </div>
                 <div className="flex w-[190px] h-[80px] items-center justify-between mr-[80px] p-[10px]">
                     <a href="/signup" className="text-[20px] text-custom-indigo font-bold">회원가입</a>
